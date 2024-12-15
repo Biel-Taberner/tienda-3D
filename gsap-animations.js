5b72304cf2dce6e328fe58d16b4eb478ab01c833
Biel-Taberner <gtaberner14592@alumnes.iesmanacor.cat>
2024-12-15 16:30:56 +0100

    Updates

--- gsap-animations.js
+++ gsap-animations.js
@@ -1,4 +1,4 @@
-import gsap from "../../node_modules/gsap/index.js";
+import gsap from "/tienda-3D/node_modules/gsap/index.js";
 import Letterize from "../node_modules/letterizejs"
 import ScrollTrigger from "../node_modules/gsap/ScrollTrigger.js";
 
